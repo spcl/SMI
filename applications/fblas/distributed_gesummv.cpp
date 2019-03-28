53f128b6e29c56ff44d663efcb80664b9c01db94
Tiziano De Matteis <tiziano.dematteis@inf.ethz.ch>
2019-03-28 19:34:35 +0100

    [SRC][ADD] Gesummv

---
+++ applications/fblas/distributed_gesummv.cpp
@@ -0,0 +1,366 @@
+/**
+ *  ATTENTION: helpers modified to have better performance (volatile, N %64 ==0 ...)
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <iostream>
+#include <string>
+#include <cblas.h>
+#include "../../include/utils/ocl_utils.hpp"
+#include "../../include/utils/utils.hpp"
+
+using namespace std;
+float *A,*B,*x,*y;
+float *fpga_res_y;
+//#define BLOCKING
+void generate_float_matrix(float *A,int N,int M)
+{
+    for(int i=0;i<N;i++)
+    {
+        for(int j=0;j<M;j++)
+           A[i*M+j] = i;//  static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/10.0));
+    }
+}
+
+void generate_float_vector (float *x, int N)
+{
+    for(int i=0;i<N;i++)
+        x[i] = i;// static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/1.0));
+//        x[i]=1  ;
+}
+
+
+
+const double flteps = 1e-4;
+
+inline bool test_equals(float result, float expected, float relative_error)
+{
+    //check also that the parameters are numbers
+    return result==result && expected ==expected && ((result==0 && expected ==0) || fabs(result-expected)/fabs(expected) < relative_error);
+}
+
+int main(int argc, char *argv[])
+{
+
+    //command line argument parsing
+    if(argc<15)
+    {
+        cerr << "Usage: "<< argv[0]<<" -b <binary file> -n <n> -a <alpha> -c <beta> -r <num runs> -k <rank 0/1> -f <fpga>"<<endl;
+        exit(-1);
+    }
+
+    int c;
+    int n,m,runs,rank;
+    double alpha,beta;
+    std::string program_path,program_path_streamed;;
+    std::string json_path;
+    int fpga;
+
+    while ((c = getopt (argc, argv, "n:b:r:a:c:k:f:")) != -1)
+        switch (c)
+        {
+            case 'n':
+                n=atoi(optarg);
+                break;
+            case 'a':
+                alpha=atof(optarg);
+                break;
+            case 'c':
+                beta=atof(optarg);
+                break;
+            case 'b':
+                program_path=std::string(optarg);
+                break;
+            case 'f':
+                fpga=atoi(optarg);
+                break;
+            case 'r':
+                runs=atoi(optarg);
+                break;
+            case'k':
+                {
+                    rank=atoi(optarg);
+                    if(rank!=0 && rank!=1)
+                    {
+                        cerr << "Error: rank may be 0-1"<<endl;
+                        exit(-1);
+                    }
+
+                    break;
+                }
+            default:
+                cerr << "Usage: "<< argv[0]<<""<<endl;
+                exit(-1);
+        }
+
+    //set affinity
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset);
+    CPU_SET(0, &cpuset);
+
+    if (pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset)) {
+        cerr << "Cannot set thread to CPU " << 0<< endl;
+    }
+
+    //create data
+    posix_memalign ((void **)&A, IntelFPGAOCLUtils::AOCL_ALIGNMENT, n*n*sizeof(float));
+    posix_memalign ((void **)&B, IntelFPGAOCLUtils::AOCL_ALIGNMENT, n*n*sizeof(float));
+    posix_memalign ((void **)&x, IntelFPGAOCLUtils::AOCL_ALIGNMENT, n*sizeof(float));
+    posix_memalign ((void **)&y, IntelFPGAOCLUtils::AOCL_ALIGNMENT, n*sizeof(float));
+    generate_float_vector(x,n);
+    generate_float_matrix(A,n,n);
+    generate_float_matrix(B,n,n);
+
+    std::vector<double> streamed_times,transfer_times;
+
+    cl::Platform platform;
+    cl::Device device;
+    cl::Context context;
+    cl::Program program;
+    std::vector<cl::Kernel> kernels;
+    std::vector<cl::CommandQueue> queues;
+    std::vector<std::string> kernel_names;
+
+
+    switch(rank)
+    {
+        case 0:
+            cout << "Starting rank 0 on FPGA "<<fpga<<endl;
+            //we don't need to start all the CKs in this case
+
+            kernel_names.push_back("gemv");
+            kernel_names.push_back("axpy");
+            kernel_names.push_back("kernel_read_matrix_A_0");
+            kernel_names.push_back("kernel_read_vector_x_0");
+            kernel_names.push_back("kernel_read_vector_y_0");
+            kernel_names.push_back("kernel_write_vector_0");
+            kernel_names.push_back("CK_S_0");
+            kernel_names.push_back("CK_R_0");
+            break;
+        case 1:
+            cout << "Starting rank 1 on FPGA "<<fpga<<endl;
+            kernel_names.push_back("gemv");
+            kernel_names.push_back("kernel_read_matrix_A_0");
+            kernel_names.push_back("kernel_read_vector_x_0");
+            kernel_names.push_back("kernel_read_vector_y_0");
+            kernel_names.push_back("CK_S_0");
+            kernel_names.push_back("CK_R_0");
+            break;
+    }
+    IntelFPGAOCLUtils::initEnvironment(platform,device,fpga,context,program,program_path,kernel_names,kernels,queues);
+    int tile_size=128;
+    int ranks=2;
+    cout << "Executing with tile size " << tile_size<<endl;
+    //TODO fix this
+    cl::Buffer input_x(context, CL_MEM_READ_ONLY|CL_CHANNEL_3_INTELFPGA, n *sizeof(float));
+    cl::Buffer output_y(context, CL_MEM_READ_WRITE|CL_CHANNEL_4_INTELFPGA, n * sizeof(float));
+    size_t elem_per_module=n*n/2;
+    cl::Buffer input_M_0(context, CL_MEM_READ_ONLY|CL_CHANNEL_1_INTELFPGA, elem_per_module*sizeof(float));
+    cl::Buffer input_M_1(context, CL_MEM_READ_ONLY|CL_CHANNEL_2_INTELFPGA, elem_per_module*sizeof(float));
+    cl::Buffer routing_table_ck_s_0(context,CL_MEM_READ_ONLY,2);
+    cl::Buffer routing_table_ck_r_0(context,CL_MEM_READ_ONLY,1);
+    if(rank==0)
+    {
+        //senders routing tables
+        char rt_s_0[2]={1,2}; //this one is useless
+        queues[0].enqueueWriteBuffer(routing_table_ck_s_0, CL_TRUE,0,2,rt_s_0);
+        //receivers routing tables: these are meaningless
+        char rt_r_0[1]={4}; //the first  is connect to application endpoint (TAG=0)
+        queues[0].enqueueWriteBuffer(routing_table_ck_r_0, CL_TRUE,0,1,rt_r_0);
+
+        //prepare data
+        //copy the matrix interleaving it into two modules
+        size_t offset=0;
+        size_t increment=64/2*sizeof(float);
+        const int loop_it=((int)(n))/64;   //W must be a divisor of M
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<loop_it;j++)
+            {
+                //write to the different banks
+                queues[0].enqueueWriteBuffer(input_M_0, CL_FALSE,offset, (64/2)*sizeof(float),&(A[i*n+j*64]));
+                queues[0].enqueueWriteBuffer(input_M_1, CL_FALSE,offset, (64/2)*sizeof(float),&(A[i*n+j*64+32]));
+
+                offset+=increment;
+            }
+        }
+        queues[0].finish();
+        queues[0].enqueueWriteBuffer(input_x,CL_TRUE,0,n*sizeof(float),x);
+        queues[0].finish();
+        printf("Data copied\n");
+
+        //gemv_A
+        int one=1;
+        int zero=0;
+        float fzero=0,fone=1;
+        int x_repetitions=ceil((float)(n)/tile_size);
+
+        kernels[0].setArg(0, sizeof(int),&one);
+        kernels[0].setArg(1, sizeof(int),&n);
+        kernels[0].setArg(2, sizeof(int),&n);
+        kernels[0].setArg(3, sizeof(float),&alpha);
+        kernels[0].setArg(4, sizeof(float),&fzero);
+
+        //axpy
+        kernels[1].setArg(0, sizeof(float),&fone);
+        kernels[1].setArg(1, sizeof(int),&n);
+
+        //read_matrix_A
+        kernels[2].setArg(0, sizeof(cl_mem),&input_M_0);
+        kernels[2].setArg(1, sizeof(cl_mem),&input_M_1);
+        kernels[2].setArg(2, sizeof(int),&n);
+        kernels[2].setArg(3, sizeof(int),&n);
+        kernels[2].setArg(4, sizeof(int),&n);
+
+
+        //read_vector_x
+        kernels[3].setArg(0, sizeof(cl_mem),&input_x);
+        kernels[3].setArg(1, sizeof(int),&n);
+        kernels[3].setArg(2, sizeof(int),&tile_size);
+        kernels[3].setArg(3, sizeof(int),&x_repetitions);
+
+        //read_vector_y
+        kernels[4].setArg(0, sizeof(cl_mem),&output_y);
+        kernels[4].setArg(1, sizeof(int),&zero);
+        kernels[4].setArg(2, sizeof(int),&zero);
+        kernels[4].setArg(3, sizeof(int),&zero);
+
+
+
+        //write_vector
+        kernels[5].setArg(0, sizeof(cl_mem),&output_y);
+        kernels[5].setArg(1, sizeof(int),&n);
+        kernels[5].setArg(2, sizeof(int),&tile_size);
+
+        //set Arguments for CKS
+        //args for the CK_Ss
+        kernels[6].setArg(0,sizeof(cl_mem),&routing_table_ck_s_0);
+        kernels[6].setArg(1,sizeof(char),&ranks);
+
+        //args for the CK_Rs
+        kernels[7].setArg(0,sizeof(cl_mem),&routing_table_ck_r_0);
+        kernels[7].setArg(1,sizeof(char),&rank);
+        kernels[7].setArg(2,sizeof(char),&ranks);
+    }
+    else
+    {
+        //senders routing tables
+        char rt_s_0[2]={0,1}; //we go to I/O for sending to rank 0
+        queues[0].enqueueWriteBuffer(routing_table_ck_s_0, CL_TRUE,0,2,rt_s_0);
+        //receivers routing tables: these are meaningless
+        char rt_r_0[1]={100}; //this one is useless
+        queues[0].enqueueWriteBuffer(routing_table_ck_r_0, CL_TRUE,0,1,rt_r_0);
+
+
+        //copy the matrix interleaving it into two modules
+        size_t offset=0;
+        size_t increment=64/2*sizeof(float);
+        const int loop_it=((int)(n))/64;   //W must be a divisor of M
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<loop_it;j++)
+            {
+                //write to the different banks
+                queues[0].enqueueWriteBuffer(input_M_0, CL_FALSE,offset, (64/2)*sizeof(float),&(B[i*n+j*64]));
+                queues[0].enqueueWriteBuffer(input_M_1, CL_FALSE,offset, (64/2)*sizeof(float),&(B[i*n+j*64+32]));
+
+                offset+=increment;
+            }
+        }
+        queues[0].finish();
+        queues[0].enqueueWriteBuffer(input_x,CL_TRUE,0,n*sizeof(float),x);
+        queues[0].finish();
+        printf("Data copied\n");
+
+        //gemv_A
+        int one=1;
+        int zero=0;
+        float fzero=0,fone=1;
+        int x_repetitions=ceil((float)(n)/tile_size);
+
+        kernels[0].setArg(0, sizeof(int),&one);
+        kernels[0].setArg(1, sizeof(int),&n);
+        kernels[0].setArg(2, sizeof(int),&n);
+        kernels[0].setArg(3, sizeof(float),&alpha);
+        kernels[0].setArg(4, sizeof(float),&fzero);
+
+
+        //read_matrix_B
+        kernels[1].setArg(0, sizeof(cl_mem),&input_M_0);
+        kernels[1].setArg(1, sizeof(cl_mem),&input_M_1);
+        kernels[1].setArg(2, sizeof(int),&n);
+        kernels[1].setArg(3, sizeof(int),&n);
+        kernels[1].setArg(4, sizeof(int),&n);
+
+
+        //read_vector_x
+        kernels[2].setArg(0, sizeof(cl_mem),&input_x);
+        kernels[2].setArg(1, sizeof(int),&n);
+        kernels[2].setArg(2, sizeof(int),&tile_size);
+        kernels[2].setArg(3, sizeof(int),&x_repetitions);
+
+        //read_vector_y useless
+        kernels[3].setArg(0, sizeof(cl_mem),&output_y);
+        kernels[3].setArg(1, sizeof(int),&zero);
+        kernels[3].setArg(2, sizeof(int),&zero);
+        kernels[3].setArg(3, sizeof(int),&zero);
+
+
+        //set Arguments for CKS
+        //args for the CK_Ss
+        kernels[4].setArg(0,sizeof(cl_mem),&routing_table_ck_s_0);
+        kernels[4].setArg(1,sizeof(char),&ranks);
+
+        //args for the CK_Rs
+        kernels[5].setArg(0,sizeof(cl_mem),&routing_table_ck_r_0);
+        kernels[5].setArg(1,sizeof(char),&rank);
+        kernels[5].setArg(2,sizeof(char),&ranks);
+
+    }
+
+    cout << "Ready to start " <<endl;
+
+    timestamp_t start=current_time_usecs();
+
+    for(int i=0;i<kernel_names.size();i++)
+        queues[i].enqueueTask(kernels[i]);
+    for(int i=0;i<kernel_names.size()-2;i++)
+        queues[i].finish();
+
+    timestamp_t end=current_time_usecs();
+    cout << "Sleeping a bit "<<endl;
+    sleep(1);
+    if(rank ==0)
+    {
+         queues[0].enqueueReadBuffer(output_y,CL_FALSE,0,n*sizeof(float),fpga_res_y);
+        //check
+        timestamp_t comp_start=current_time_usecs();
+        cblas_sgemv(CblasRowMajor,CblasNoTrans,n,n,beta,B,n,x,1,0,y,1);
+        cblas_sgemv(CblasRowMajor,CblasNoTrans,n,n,alpha,A,n,x,1,1,y,1);
+        timestamp_t cpu_time=current_time_usecs()-comp_start;
+
+
+        //forse meglio passare a norma
+        bool ok=true;
+
+        for(int i=0;i<n;i++)
+        {
+            if(!test_equals(fpga_res_y[i],y[i],flteps))
+            {
+                ok=false;
+                cout << i << " Error streamed: " <<fpga_res_y[i]<<" != " <<y[i]<<endl;
+            }
+        }
+
+
+        if(ok )
+            std::cout <<"OK!!!" <<std::endl;
+        else
+            std::cout <<"ERROR!!!" <<std::endl;
+
+        cout << "Computation time (usecs): " <<end-start <<endl;
+    }
+
+}
---
+++ applications/fblas/distributed_gesummv_rank0.cl
@@ -0,0 +1,330 @@
+/**
+    Distributed implementation of GESUMMV
+
+    RANK-0 will compute y'=alpha*A*x.
+    This, will be combined with the result of RANK-1 (beta*A*x)
+    for producing the final result
+
+    GEMV has access to 2 memory modules.
+
+*/
+
+
+#pragma OPENCL EXTENSION cl_intel_channels : enable
+
+#define W 64
+#define TILE_N 128
+#define TILE_M 128
+#define INCX 1
+#define INCY 1
+#define INCW 1
+#define KERNEL_NAME gemv
+#define CHANNEL_VECTOR_X channel_in_vector_x_0
+#define CHANNEL_VECTOR_Y channel_in_vector_y_0
+#define CHANNEL_MATRIX_A channel_in_matrix_A_0
+#define CHANNEL_VECTOR_OUT channel_out_vector_0
+#define CHANNEL_RESULT channel_result
+#define READ_VECTOR_X kernel_read_vector_x_0
+#define READ_VECTOR_Y kernel_read_vector_y_0
+#define READ_MATRIX_A kernel_read_matrix_A_0
+#define WRITE_VECTOR kernel_write_vector_0
+#define __STRATIX_10__
+#include "smi_rank0.h"
+#include <commons.h>
+#if (TILE_N>TILE_M)
+    #define MAX_TILE_SIZE TILE_N
+#else
+    #define MAX_TILE_SIZE TILE_M
+#endif
+
+
+channel TYPE_T CHANNEL_VECTOR_X __attribute__((depth(W)));
+channel TYPE_T CHANNEL_VECTOR_Y __attribute__((depth(W)));
+channel TYPE_T CHANNEL_MATRIX_A __attribute__((depth(W)));
+channel TYPE_T CHANNEL_VECTOR_OUT __attribute__((depth(W)));
+channel TYPE_T CHANNEL_RESULT __attribute__((depth(W)));
+
+
+__kernel void KERNEL_NAME(int row_streamed, const int N, const int M, const TYPE_T alpha, const TYPE_T beta)
+{
+
+    int len_x,tile_x;
+    int len_y,tile_y;
+    int BlocksX, BlocksY;
+    //chose the loop limits
+    if(row_streamed == 1)
+    {
+        len_x = M;
+        len_y = N;
+        tile_x=TILE_M;
+        tile_y=TILE_N;
+        BlocksY=1+(int)((N-1)/TILE_N); //ceiling
+        BlocksX=1+(int)((M-1)/TILE_M);
+    }
+    else
+    {	//in this case A is transposed
+        len_x = N;
+        len_y = M;
+        tile_x=TILE_N;
+        tile_y=TILE_M;
+        BlocksY=1+(int)((M-1)/TILE_M);
+        BlocksX=1+(int)((N-1)/TILE_N);
+    }
+
+    #ifdef DOUBLE_PRECISION
+    TYPE_T shift_reg[SHIFT_REG+1]; //shift register
+
+    for(int i=0;i<SHIFT_REG+1;i++)
+       shift_reg[i]=0;
+    #endif
+
+
+    //The computation is performed by receiving A in tiles by row (A non transposed) or column (A transposed).
+    //In this way, the result is computed by 'accumulating' over y elements
+    //One block of y is computed for each row-tile (or column-tile) of A and using the entire x
+
+    const int computing_outer_loop_limit=(int)(tile_x/W);
+    const int reading_y_outer_loop_limit=(int)(tile_y/W);
+
+    //Please note: the order in which tiles arrive, will determine the computation
+    //(i.e. do not assume that you will receive the tiles one row after the other...maybe they can arrive column by column)
+
+    TYPE_T local_y[MAX_TILE_SIZE];
+    TYPE_T local_x[MAX_TILE_SIZE];
+    #pragma max_concurrency 1
+    for(int ti=0;ti<BlocksY;ti++)
+    {
+        #pragma ivdep array(local_y)
+        #pragma max_concurrency 1
+        for(int tj=0;tj<BlocksX;tj++)
+        {
+
+            #pragma ivdep array(local_x)
+            for(int i=0;i<tile_y;i++)
+            {
+
+                TYPE_T acc_o=0;
+                TYPE_T acc_i=0;
+                TYPE_T prev;
+
+
+                //here we read one element from A and one element from X and we use it
+                //For X we buffer it at the first iteration
+                #pragma ivdep array(local_x)
+                for(int jj=0;jj<computing_outer_loop_limit;jj++)
+                {
+                    if(tj==0 && jj==0) //read y if this is the first iteration over the blocks of X
+                    {
+                        if(beta==0)
+                            prev=0;
+                        else
+                           prev=beta*read_channel_intel(CHANNEL_VECTOR_Y);
+                    }
+                    if(tj!=0)
+                        prev=local_y[i];
+
+                    if(i==0) //receive x
+                    {
+                        #pragma unroll
+                        for(int j=0;j<W;j++)
+                           local_x[jj*W+j]=read_channel_intel(CHANNEL_VECTOR_X);
+                    }
+                    acc_i=0;
+                    //read (a block of W elements) of the row of A
+
+                    //receive elemnts of a: decoupling this from the computation loop
+                    //maybe usefulein case the sender of A does not perform unrolled writes into the channel
+                    TYPE_T local_A[W];
+                    #pragma unroll
+                    for(int j=0;j<W;j++)
+                        local_A[j]=read_channel_intel(CHANNEL_MATRIX_A);
+
+                    #pragma unroll
+                    for(int j=0;j<W;j++)
+                        acc_i+=local_A[j]*local_x[jj*W+j];
+
+                    #ifdef DOUBLE_PRECISION
+                        shift_reg[SHIFT_REG] = shift_reg[0]+alpha*acc_i;
+                        //Shift every element of shift register
+                        #pragma unroll
+                        for(int j = 0; j < SHIFT_REG; ++j)
+                            shift_reg[j] = shift_reg[j + 1];
+                    #else
+                        acc_o+=alpha*acc_i;
+                    #endif
+
+                }
+                #ifdef DOUBLE_PRECISION
+                    //reconstruct the result using the partial results in shift register
+                    #pragma unroll
+                    for(int i=0;i<SHIFT_REG;i++)
+                    {
+                        acc_o+=shift_reg[i];
+                        shift_reg[i]=0;
+                    }
+                #endif
+                TYPE_T result =  prev+ acc_o;
+                local_y[i] = result;
+
+                //output y if we reached the end of the matrix
+                //y is output one element at a time
+                if(tj==BlocksX-1)
+                   write_channel_intel(CHANNEL_VECTOR_OUT,result);
+            }
+        }
+    }
+}
+
+
+__kernel void axpy(const TYPE_T alpha, int N)
+{
+    //We don't need unroll here: data is generated by the previous
+    //GEMV one element at a time
+    if(N==0) return;
+
+    const int outer_loop_limit=1+(int)((N-1)/W); //ceiling
+    TYPE_T res[W];
+    SMI_Channel chan=SMI_Open_receive_channel(N,SMI_FLOAT,1,0);
+
+    for(int i=0;i<N;i++)
+    {
+        float rcvd_rank1;
+        SMI_Pop(&chan, &rcvd_rank1);
+        float rcvd_rank0=read_channel_intel(CHANNEL_VECTOR_OUT);
+        write_channel_intel(CHANNEL_RESULT,rcvd_rank0+rcvd_rank1);
+    }
+
+
+}
+
+__kernel void READ_VECTOR_X(__global volatile TYPE_T *restrict data, unsigned int N, unsigned int pad_size, unsigned int repetitions)
+{
+    unsigned int ratio=pad_size/W;
+
+    unsigned int padding_loop_limit=ceil(((float)N)/pad_size);
+    unsigned int outer_loop_limit=padding_loop_limit*ratio;
+    TYPE_T x[W];
+    for(int t=0; t< repetitions;t++)
+    {
+        //compute the starting index
+        int offset=((INCX) > 0 ?  0 : ((N) - 1) * (-(INCX)));
+
+        for(int i=0;i<outer_loop_limit;i++)
+        {
+            //prepare data
+            #pragma unroll
+            for(int k=0;k<W;k++)
+            {
+                if(i*W+k<N)
+                    x[k]=data[offset+(k*INCX)];
+                else
+                    x[k]=0;
+                write_channel_intel(CHANNEL_VECTOR_X,x[k]);
+            }
+            offset+=W*INCX;
+
+        }
+    }
+}
+
+
+__kernel void READ_VECTOR_Y(__global  volatile TYPE_T *restrict data, unsigned int N, unsigned int pad_size, unsigned int repetitions)
+{
+
+    unsigned int ratio=pad_size/W;
+    unsigned int padding_loop_limit=ceil(((float)N)/pad_size);
+    unsigned int outer_loop_limit=padding_loop_limit*ratio;
+    TYPE_T y[W];
+    for(int t=0; t< repetitions;t++)
+    {
+        //compute the starting index
+        int offset=((INCY) > 0 ?  0 : ((N) - 1) * (-(INCY)));
+        for(int i=0;i<outer_loop_limit;i++)
+        {
+            //prepare data
+            #pragma unroll
+            for(int k=0;k<W;k++)
+            {
+                if(i*W+k<N)
+                    y[k]=data[offset+(k*INCY)];
+                else
+                    y[k]=0;
+            }
+            offset+=W*INCY;
+            #pragma unroll
+            for(int k=0;k<W;k++)
+                write_channel_intel(CHANNEL_VECTOR_Y,y[k]);
+        }
+    }
+}
+
+
+
+//N and M must be a multiple of 64
+__kernel void READ_MATRIX_A(__global volatile  float *restrict data0,__global volatile  float *restrict data1, int N, int M, unsigned int lda)
+{
+    const int BlocksN=1+(int)((N-1)/TILE_N);
+    const int BlocksM=1+(int)((M-1)/TILE_M);
+    const int loop_it=((int)(TILE_M))/W;   //W must be a divisor of TILE
+    const int multiply64=1+(int)((lda-1)/64); //lda must be a multiple of 64, otherwise inefficient hw is generated for the load
+
+    float to_send[W];
+
+    for(int ti=0;ti<BlocksN;ti++)
+    {
+        for(int tj=0;tj<BlocksM;tj++)
+        {
+            for(int i=0;i<TILE_N;i++)
+            {
+                for(int j=0;j<loop_it;j++)
+                {
+                    const int row_idx=ti*TILE_N+i;
+                    //load from memory
+
+                    #pragma unroll
+                    for(int k=0;k<32;k++)
+                            to_send[k]=data0[row_idx*32*multiply64+tj*TILE_M/2+j*32+k];
+
+                    #pragma unroll
+                    for(int k=0;k<32;k++)
+                            to_send[k+32]=data1[row_idx*32*multiply64+tj*TILE_M/2+j*32+k];
+
+                    #pragma unroll
+                    for(int k = 0; k < W; k++)
+                        write_channel_intel(CHANNEL_MATRIX_A,to_send[k]);
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+__kernel void WRITE_VECTOR(__global volatile TYPE_T *restrict out, unsigned int N,unsigned int pad_size)
+{
+    const unsigned int ratio=pad_size/W;
+    const unsigned int padding_loop_limit=ceil(((float)N)/pad_size);
+    const unsigned int outer_loop_limit=padding_loop_limit*ratio;
+    TYPE_T recv[W];
+    //compute the starting index
+    int offset=((INCW) > 0 ?  0 : ((N) - 1) * (-(INCW)));
+    //receive and store data into memory
+    for(int i=0;i<outer_loop_limit;i++)
+    {
+        #pragma unroll
+        for(int j=0;j<W;j++)
+        {
+            recv[j]=read_channel_intel(CHANNEL_RESULT);
+
+            if(i*W+j<N)
+                out[offset+(j*INCW)]=recv[j];
+        }
+        offset+=W*INCW;
+    }
+}
+
