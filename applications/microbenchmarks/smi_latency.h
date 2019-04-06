fd55dd1d0e67b5f4553768e05114f2780fa247c8
Tiziano De Matteis <tdematt@localhost.localdomain>
2019-04-06 10:53:50 +0200

    [SRC][UPD] Latency with channels of size 1

--- applications/microbenchmarks/smi_latency.h
+++ applications/microbenchmarks/smi_latency.h
@@ -6,7 +6,7 @@
 #pragma OPENCL EXTENSION cl_intel_channels : enable
 #pragma OPENCL EXTENSION cl_khr_fp64 : enable
 
-#include "../../kernels/communications/channel_helpers.h"
+#include "smi/channel_helpers.h"
 
 
 
